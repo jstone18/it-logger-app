b7b68a75097e26ee61bfdecda72a9a3dc5f50942
jstone18 <jason.stone0808@gmail.com>
2020-02-25 17:41:35 -0500

    Create action and action types. Update logReducer. Refactor Log component with redux functionality

--- src/redux/reducers/logReducer.js
+++ src/redux/reducers/logReducer.js
@@ -1,3 +1,5 @@
+import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "../actions/types";
+
 const INITIAL_STATE = {
 	logs: null,
 	currrent: null,
@@ -7,6 +9,23 @@ const INITIAL_STATE = {
 
 export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
+		case GET_LOGS:
+			return {
+				...state,
+				logs: action.payload,
+				loading: false
+			};
+		case SET_LOADING:
+			return {
+				...state,
+				loading: true
+			};
+		case LOGS_ERROR:
+			console.error(action.payload);
+			return {
+				...state,
+				error: action.payload
+			};
 		default:
 			return state;
 	}
