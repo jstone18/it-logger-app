04c7f2430e7ed5c4b0a3774602aae7c6758eacb8
jstone18 <jason.stone0808@gmail.com>
2020-02-26 00:59:02 -0500

    Create addLog action and reducer. Update AddLogModal

--- src/redux/actions/logActions.js
+++ src/redux/actions/logActions.js
@@ -1,5 +1,6 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from "./types";
 
+// Get logs from server
 export const getLogs = () => async dispatch => {
 	try {
 		setLoading();
@@ -19,7 +20,33 @@ export const getLogs = () => async dispatch => {
 	}
 };
 
-// set loading to true
+// Add new log
+export const addLog = log => async dispatch => {
+	try {
+		setLoading();
+
+		const res = await fetch("/logs", {
+			method: "POST",
+			body: JSON.stringify(log),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		});
+		const data = await res.json();
+
+		dispatch({
+			type: ADD_LOG,
+			payload: data
+		});
+	} catch (error) {
+		dispatch({
+			type: LOGS_ERROR,
+			payload: error.response.data
+		});
+	}
+};
+
+// Set loading to true
 export const setLoading = () => {
 	return {
 		type: SET_LOADING
--- src/redux/reducers/logReducer.js
+++ src/redux/reducers/logReducer.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "../actions/types";
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from "../actions/types";
 
 const INITIAL_STATE = {
 	logs: null,
@@ -13,6 +13,12 @@ export default (state = INITIAL_STATE, action) => {
 			return {
 				...state,
 				logs: action.payload,
+				loading: false
+			};
+		case ADD_LOG:
+			return {
+				...state,
+				logs: [...state.logs, action.payload],
 				loading: false
 			};
 		case SET_LOADING:
