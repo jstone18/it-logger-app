800f7bdc62fa7174d4d861b0588b1e59924c4e82
jstone18 <jason.stone0808@gmail.com>
2020-02-28 16:57:00 -0700

    Create TechSelectOptions for AddLog and EditLog

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { addLog } from "../../redux/actions/logActions";
 import M from "materialize-css/dist/js/materialize.min.js";
+import TechSelectOptions from "../techs/TechSelectOptions";
 
 const AddLogModal = ({ addLog }) => {
 	const [message, setMessage] = useState("");
@@ -61,9 +62,7 @@ const AddLogModal = ({ addLog }) => {
 							<option value="" disabled>
 								Select Technician
 							</option>
-							<option value="Jon Doe">Jon Doe</option>
-							<option value="Sam Smith">Sam Smith</option>
-							<option value="Sara Wilson">Sara Wilson</option>
+							<TechSelectOptions />
 						</select>
 					</div>
 				</div>
--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { updateLog } from "../../redux/actions/logActions";
 import M from "materialize-css/dist/js/materialize.min.js";
 import PropTypes from "prop-types";
+import TechSelectOptions from "../techs/TechSelectOptions";
 
 const EditLogModal = ({ updateLog, current }) => {
 	const [message, setMessage] = useState("");
@@ -63,9 +64,7 @@ const EditLogModal = ({ updateLog, current }) => {
 							<option value="" disabled>
 								Select Technician
 							</option>
-							<option value="Jon Doe">Jon Doe</option>
-							<option value="Sam Smith">Sam Smith</option>
-							<option value="Sara Wilson">Sara Wilson</option>
+							<TechSelectOptions />
 						</select>
 					</div>
 				</div>
